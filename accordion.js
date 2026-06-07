dda4e057643d2c5743890524238ec229436767ec
Gerardo Cianciulli <gerardo.cianciulli@gmail.com>
2026-06-07 11:20:05 -0400

    enable keyboard navigation

--- accordion.js
+++ accordion.js
@@ -1,14 +1,16 @@
-$(document).ready(function(){
-    var arrowSVG = '<svg width="10" height="7" xmlns="http://www.w3.org/2000/svg"><path d="M1 .799l4 4 4-4" stroke="#F47B56" stroke-width="2" fill="none" fill-rule="evenodd"/></svg>'
-    
-    $(".question").append(arrowSVG);
-    $(".question-group").click(function(e){
-      var expandAnswer = !$(e.target).hasClass("selected")
-      // Find element with class "selected" and remove it
-      // If this has class "selected" it will contrct the group
-      $(".selected").removeClass("selected")
-      // Otherwise it will expand the group 
-      if (expandAnswer)
-        $("#" + e.target.id).addClass("selected")
-    });
-  });
+$(document).ready(function () {
+  const arrowSVG =
+    '<svg width="10" height="7" xmlns="http://www.w3.org/2000/svg"><path d="M1 .799l4 4 4-4" stroke="#F47B56" stroke-width="2" fill="none" fill-rule="evenodd"/></svg>';
+
+  const expandContractGroup = (e) => {
+    const expandAnswer = !$(e.target).hasClass("selected");
+    // Find element with class "selected" and remove it
+    // If this has class "selected" it will contrct the group
+    $(".selected").removeClass("selected");
+    // Otherwise it will expand the group
+    if (expandAnswer) $("#" + e.target.id).addClass("selected");
+  };
+
+  $(".question").append(arrowSVG);
+  $(".question-group").focus(expandContractGroup);
+});
